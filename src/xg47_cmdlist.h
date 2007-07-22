3c8e94c4dc2f3e10bad4c8c7a7e87c232a0d775d
Ian Romanick <idr@us.ibm.com>
2007-07-21 23:07:57 -0700

    Fix a s/CARD32/uint32_t/ change missed on the previous commit.

--- src/xg47_cmdlist.h
+++ src/xg47_cmdlist.h
@@ -33,7 +33,8 @@ struct xg47_CmdList;
 /* cmdList size in dword */
 extern int  xg47_BeginCmdList(struct xg47_CmdList *, unsigned int size);
 extern void xg47_EndCmdList(struct xg47_CmdList *);
-extern void xg47_SendGECommand(struct xg47_CmdList *, CARD32 addr, CARD32 cmd);
+extern void xg47_SendGECommand(struct xg47_CmdList *, uint32_t addr,
+    uint32_t cmd);
 
 extern struct xg47_CmdList *xg47_Initialize(ScrnInfoPtr pScrn, 
     unsigned int cmdBufSize, int fd);
