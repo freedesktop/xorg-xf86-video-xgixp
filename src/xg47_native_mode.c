6f52c837fa601135329743b9f38dafbb09a4f907
Ian Romanick <idr@us.ibm.com>
2008-01-18 16:21:46 -0800

    Use table for VCLK values and fix bug in horiz param overflow register setting
    These changes fix most 1600x1200 (and higher) problems.

--- src/xg47_native_mode.c
+++ src/xg47_native_mode.c
@@ -28,12 +28,15 @@
 #endif
 
 #include "xgi.h"
+
+#ifdef NATIVE_MODE_SETTING
 #include "xgi_driver.h"
 #include "xgi_regs.h"
 #include "xgi_bios.h"
 #include "xgi_mode.h"
 #include "xg47_mode.h"
 
+#if 0
 #define FREQ_FROM_NMK(_n, _m, _k) \
     ((1431818 * (_n + 8) / ((_m + 1) << (_k))) / 100)
 
@@ -97,6 +100,112 @@ static void SetVCLK(XGIPtr pXGI, DisplayModePtr disp_mode)
     OUT3C5B(0x18, n);
     OUT3C5B(0x19, ((k << 6) & 0xc0) | (m & 0x3f));
 }
+#else
+struct vclk_tab {
+	unsigned freq;
+	uint8_t  sr18;
+	uint8_t  sr19;
+	uint8_t  p;
+};
+
+static const struct vclk_tab clk[] = {
+	{ 25175, 0xCB, 0x9D, 0x00 },
+	{ 28322, 0xAE, 0x96, 0x00 },
+	{ 31500, 0x24, 0x84, 0x00 },
+	{ 32668, 0x8A, 0x8F, 0x00 },
+	{ 36000, 0xAE, 0xC8, 0x22 },
+	{ 40000, 0x73, 0x8A, 0x66 },
+	{ 43163, 0xC5, 0x90, 0x66 },
+	{ 44900, 0x82, 0x8A, 0x66 },
+	{ 49500, 0x4B, 0x85, 0x66 },
+	{ 50350, 0xCB, 0x8E, 0x44 },
+	{ 51000, 0x31, 0x83, 0x44 },
+	{ 52406, 0xC5, 0x8D, 0x44 },
+	{ 56250, 0x66, 0x86, 0x44 },
+	{ 65000, 0x65, 0x85, 0x44 },
+	{ 68179, 0xF0, 0x8C, 0x44 },
+	{ 71000, 0x6F, 0x85, 0x33 },
+	{ 74160, 0x89, 0x86, 0x33 },
+	{ 75000, 0xDE, 0x8A, 0x33 },
+	{ 78750, 0x24, 0x81, 0x22 },
+	{ 79500, 0x67, 0x84, 0x44 },
+	{ 81750, 0x81, 0x85, 0x22 },
+	{ 83462, 0xCA, 0x88, 0x33 },
+	{ 83950, 0xCB, 0x88, 0x22 },
+	{ 90000, 0xA8, 0x86, 0x33 },
+	{ 94500, 0x7C, 0x84, 0x33 },
+	{ 96307, 0x8C, 0x4A, 0x66 },
+	{ 99000, 0x4B, 0x82, 0x33 },
+	{ 101250, 0xBE, 0x86, 0x33 },
+	{ 106472, 0x6F, 0x83, 0x33 },
+	{ 108000, 0xAD, 0x85, 0x22 },
+	{ 112500, 0xD4, 0x86, 0x22 },
+	{ 113309, 0x57, 0x82, 0x22 },
+	{ 119651, 0xE2, 0x86, 0x22 },
+	{ 122500, 0xA3, 0x84, 0x22 },
+	{ 124090, 0x60, 0x82, 0x22 },
+	{ 129000, 0xAC, 0x84, 0x22 },
+	{ 130295, 0xAE, 0x84, 0x22 },
+	{ 133000, 0xD7, 0x4B, 0x44 },
+	{ 135000, 0x7C, 0x46, 0x44 },
+	{ 139054, 0xE1, 0x85, 0x22 },
+	{ 146250, 0x87, 0x46, 0x44 },
+	{ 148250, 0xC7, 0x84, 0x22 },
+	{ 154250, 0xE5, 0x4A, 0x33 },
+	{ 155750, 0xA6, 0x83, 0x22 },
+	{ 157500, 0xA8, 0x83, 0x22 },
+	{ 160000, 0x7E, 0x82, 0x22 },
+	{ 162000, 0xDA, 0x84, 0x22 },
+	{ 164000, 0xF4, 0x4A, 0x33 },
+	{ 178000, 0xA6, 0x46, 0x33 },
+	{ 179250, 0x8E, 0x82, 0x22 },
+	{ 181000, 0xA9, 0x46, 0x22 },
+	{ 189000, 0x7C, 0x44, 0x22 },
+	{ 190960, 0x48, 0x42, 0x33 },
+	{ 193000, 0x49, 0x42, 0x33 },
+	{ 202500, 0xBE, 0x46, 0x22 },
+	{ 229500, 0x38, 0x41, 0x22 },
+	{ 234000, 0x5A, 0x42, 0x22 },
+	{ 267000, 0x68, 0x42, 0x22 },
+	{ 280637, 0xBC, 0x44, 0x22 },
+	{ 297000, 0x4B, 0x41, 0x22 },
+	{ 340477, 0x57, 0x41, 0x22 },
+};
+
+static void SetVCLK(XGIPtr pXGI, DisplayModePtr disp_mode)
+{
+	unsigned base;
+	unsigned count;
+	const unsigned num_vclk = sizeof(clk) / sizeof(clk[0]);
+
+
+	/* Select primary VCLK
+	 */
+	OUT3C5B(0x29, IN3C5B(0x29) & ~0x30);
+
+
+	base = 0;
+	count = num_vclk;
+	while (count > 1) {
+		const unsigned i = base + (count / 2);
+		if (clk[i].freq <= disp_mode->Clock) {
+			base = i;
+			count -= count / 2;
+		} else {
+			count = count / 2;
+		}
+	}
+
+	xf86DrvMsg(pXGI->pScrn->scrnIndex, X_INFO,
+		"VCLK %d -> 0x%x 0x%x 0x%x (%d)\n",
+		disp_mode->Clock, clk[base].p, clk[base].sr18, clk[base].sr19,
+		clk[base].freq);
+
+	OUT3C5B(0x28, (IN3C5B(0x28) & ~0x07) | (clk[base].p & 0x07));
+	OUT3C5B(0x18, clk[base].sr18);
+	OUT3C5B(0x19, clk[base].sr19);
+}
+#endif
 
 
 static void FillExtendedRegisters(XGIPtr pXGI, DisplayModePtr disp_mode)
@@ -241,8 +350,8 @@ static void FillExtendedRegisters(XGIPtr pXGI, DisplayModePtr disp_mode)
 
     /* Horizontal parameters overflow register.
      */
-    OUT3X5B(0x2B, ((((disp_mode->CrtcHTotal >> 3) - 5) & 0x200) >> 8)
-            | ((((disp_mode->CrtcHBlankStart >> 3) - 1) & 0x200) >> 5));
+    OUT3X5B(0x2B, ((((disp_mode->CrtcHTotal >> 3) - 5) & 0x100) >> 8)
+            | ((((disp_mode->CrtcHBlankStart >> 3) - 1) & 0x100) >> 5));
 
 
     if (disp_mode->Flags & V_INTERLACE) {
@@ -375,6 +484,8 @@ Bool XG47_NativeModeInit(ScrnInfoPtr pScrn, DisplayModePtr disp_mode)
      */
     OUT3C5B(0x24, IN3C5B(0x24) & ~0x01);
     OUT3CFB(0x23, IN3CFB(0x23) | 0x03);
+
+    OUT3X5B(0x39, 0x85);
 
     SetModeCRTC1(pXGI, disp_mode);
     SetColorDAC(pXGI, pScrn->bitsPerPixel);
@@ -529,3 +640,5 @@ void XG47_NativeModeRestore(ScrnInfoPtr pScrn, XGIRegPtr pXGIReg)
     OUT3X5B(0x13, pXGIReg->regs3X5[0x13]);
     OUT3X5B(0x8B, pXGIReg->regs3X5[0x8B]);
 }
+
+#endif /* NATIVE_MODE_SETTING */
